9256f01d8e451f1a3dabb411150ca3c539ab9d18
WilliamScript <Williampg1998@gmail.com>
2021-04-28 02:42:20 -0400

    Needs revision, finally figured out that I can't append strings, will work on it tomorrow

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 const form = document.querySelector('form')
 
 
-
 form.addEventListener('submit', function(event){
     event.preventDefault();
 
@@ -12,15 +11,33 @@ form.addEventListener('submit', function(event){
     const emailInput = document.getElementById('email').value;
     const commentInput = document.getElementById('comment').value;
     
+    
     // Create a div with the class .commentCont to hold user inputs
 
     const commentContainer = document.createElement('div');
     commentContainer.classList.add("flexComment", "commentCont");
     console.log(commentContainer);
 
-    // Put the user inputs into the comment Container
+    // Add Inputs to a variable called "new comment"
+
+    // ** I realized that I cannot appendChild with a string so this code needs a little bit of work, It took me a little bit to figure this out so I did not have enough time to complete.
+    const newComment = `
+    <div class="comment">
+        <div class="profilePic">
+            <img src="./assets/captured-final-assets/comment-2.jpg" alt="A white female smiling">
+        </div> 
+        ${commentContainer.innerHTML = `
+        <p class="date">Wednesday October 10th, 2018 by ${nameInput}</p>
+        <p>${commentInput}</p>`}
+    </div>`
+
+    console.log(newComment);
 
     // Append the comment container
+    const commentParent = document.getElementById('commentParent')
+    commentParent.appendChild(newComment);
+
+
 
     
 });
